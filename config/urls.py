c0236c7f367bb92113840e6ce0aa70976b346afc
Yang Suwon <ysw7939@gmail.com>
2023-12-07 14:39:12 +0900

    commit all

--- config/urls.py
+++ config/urls.py
@@ -1,25 +1,13 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from pybo.views import base_views
 
 # pybo/로 시작하는 페이지를 요청하면 이제 pybo/urls.py 파일의 매핑 정보를 읽어서
 # 처리하라는 의미다. 하위 파일이 생성되더라도 경로를 새로 추가할 필요가 없어 진다.
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
+    path('common/', include('common.urls')),
+    path('', base_views.index, name='index'),  # '/' 에 해당되는 path
+   
 ]
--- pybo/models.py
+++ pybo/models.py
@@ -1,13 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # 질문
 class Question(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
     # 제목 char에 200 길이
     subject = models.CharField(max_length=200)
     # 내용 길이를 가지지 않음
     content = models.TextField()
     # 날짜 DateTimeFiled를 사용
     create_date = models.DateTimeField()
+    
+    modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_question')  # 추천인 추가
 
     # id값대신 suject를 리턴하게 해줌
     def __str__(self):
@@ -15,8 +20,11 @@ class Question(models.Model):
 
 # 질문에 대한 답변에 해당됨 
 class Answer(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_answer')
     # 질문에대한 연결된 답변 ForeignKey를 이용한다.
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_answer')
 
